d6b7841d31b77aac2e4a6ddf07741ff82b0c2eb6
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2020-03-26 09:38:13 +0100

    Clean preprocessing_ter.py

--- preprocessing_ter.py
+++ preprocessing_ter.py
@@ -1,3 +1,35 @@
+# -*- coding: utf-8 -*-
+# Author: Antoine DELPLACE
+# Last update: 26/03/2020
+"""
+Third and final preprocessing program that :
+- resizes all images to the same shape
+- converts all inputs and ground truth files to one-hot encoding
+
+Requirements
+----------
+- Reduced Chargrid arrays must be located in the folder dir_np_chargrid_reduced = "./data/np_chargrids_reduced/"
+- Reduced Segmentation arrays must be located in the folder dir_np_gt_reduced = "./data/np_gt_reduced/"
+- Reduced Class bounding box dataframes must be located in the folder dir_pd_bbox_reduced = "./data/pd_bbox_reduced/"
+
+Hyperparameters
+----------
+- target_height : output height
+- target_width : output width
+- target_digit : number of output channels for chargrids
+- target_class : number of output channels for the segmentations
+- nb_anchors : number of anchors for the bounding boxes
+- nb_digit_threshold : number of digit occurrences below which it is marked as unkwown
+
+Return
+----------
+Several files are generated :
+- in outdir_np_chargrid_1h = "./data/np_chargrids_1h/" : the one-hot Chargrids in npy
+- in outdir_np_gt_1h = "./data/np_gt_1h/" : the one-hot Class Segmentations in npy
+- in outdir_np_bbox_anchor_mask = "./data/np_bbox_anchor_mask/" : the Bounding Box anchor masks in npy
+- in outdir_np_bbox_anchor_coord = "./data/np_bbox_anchor_coord/" : the Bounding Box anchor coordinates in npy
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,7 +44,6 @@ outdir_np_chargrid_1h = "./data/np_chargrids_1h/"
 outdir_np_gt_1h = "./data/np_gt_1h/"
 outdir_np_bbox_anchor_mask = "./data/np_bbox_anchor_mask/"
 outdir_np_bbox_anchor_coord = "./data/np_bbox_anchor_coord/"
-list_filenames = [f for f in os.listdir(dir_np_chargrid_reduced) if os.path.isfile(os.path.join(dir_np_chargrid_reduced, f))]
 target_height = 256
 target_width = 128
 target_digit = 61
@@ -20,98 +51,69 @@ target_class = 5
 nb_anchors = 4 # one per foreground class
 nb_digit_threshold = 10000
 
-tab_img = []
-tab_gt = []
-for i in range(0, len(list_filenames)):
-    tab_img.append(np.load(os.path.join(dir_np_chargrid_reduced, list_filenames[i])))
-    tab_gt.append(np.load(os.path.join(dir_np_gt_reduced, list_filenames[i])))
+def print_stats_img(tab_img):
+    tab_width = [np.shape(tab_img[i])[0] for i in range(0, len(tab_img))]
+    print("min width=", np.min(tab_width))
+    print("max width=", np.max(tab_width), list_filenames[np.argmax(tab_width)])
+    print("ave width=", np.average(tab_width))
+    print("std width=", np.std(tab_width))
 
-print("tab_img shape=", np.shape(tab_img))
-print("tab_gt shape=", np.shape(tab_gt))
+    tab_height = [np.shape(tab_img[i])[1] for i in range(0, len(tab_img))]
+    print("min height=", np.min(tab_height))
+    print("max height=", np.max(tab_height), list_filenames[np.argmax(tab_height)])
+    print("ave height=", np.average(tab_height))
+    print("std height=", np.std(tab_height))
 
-tab_width = [np.shape(tab_img[i])[0] for i in range(0, len(tab_img))]
-print("min width=", np.min(tab_width))
-print("max width=", np.max(tab_width), list_filenames[np.argmax(tab_width)])
-print("ave width=", np.average(tab_width))
-print("std width=", np.std(tab_width))
+def print_stats_seg(tab_gt):
+    gt_stats = [np.unique(gt, return_counts=True) for gt in tab_gt]
+    prop_nb_class = [0]*target_class
+    prop_class = [0]*target_class #5 classes (other, total, address, company, date)
+    for uniqu, count in gt_stats:
+        prop_nb_class[len(uniqu)-1] += 1
+        for i in range(0, len(uniqu)):
+            prop_class[uniqu[i]] += count[i]
 
-tab_height = [np.shape(tab_img[i])[1] for i in range(0, len(tab_img))]
-print("min height=", np.min(tab_height))
-print("max height=", np.max(tab_height), list_filenames[np.argmax(tab_height)])
-print("ave height=", np.average(tab_height))
-print("std height=", np.std(tab_height))
+    print("prop_nb_class=", prop_nb_class, prop_nb_class/np.sum(prop_nb_class))
+    print("prop_class=", prop_class, prop_class/np.sum(prop_class))
 
-gt_stats = [np.unique(gt, return_counts=True) for gt in tab_gt]
-prop_nb_class = [0, 0, 0, 0, 0]
-prop_class = [0, 0, 0, 0, 0] #5 classes (other, total, address, company, date)
-for uniqu, count in gt_stats:
-    prop_nb_class[len(uniqu)-1] += 1
-    for i in range(0, len(uniqu)):
-        prop_class[uniqu[i]] += count[i]
+def discard_digits_with_low_occurence(tab_img):
+    nb_unique_digit, count_digit = np.unique(np.concatenate([img.flatten() for img in tab_img]), return_counts=True)
+    mask_digit_to_keep = count_digit>nb_digit_threshold
 
-print("prop_nb_class")
-print(prop_nb_class)
-print(prop_nb_class/np.sum(prop_nb_class))
-print("prop_class")
-print(prop_class)
-print(prop_class/np.sum(prop_class))
-
-nb_unique_digit, count_digit = np.unique(np.concatenate([img.flatten() for img in tab_img]), return_counts=True)
-mask_digit_to_keep = count_digit>nb_digit_threshold
-print(len(nb_unique_digit))
-print(nb_unique_digit)
-print(np.sum(mask_digit_to_keep))
-print(count_digit)
-
-new_digit_nb = np.cumsum(mask_digit_to_keep)
-new_digit_nb -= 1
-print(new_digit_nb)
-for i in range(1, len(new_digit_nb)):
-    if new_digit_nb[len(new_digit_nb)-i] == new_digit_nb[len(new_digit_nb)-i-1]:
-        new_digit_nb[len(new_digit_nb)-i] = target_digit-1
-print(new_digit_nb)
-
-for i in range(0, len(list_filenames)):
-    for j in range(0, len(nb_unique_digit)):
-        tab_img[i][tab_img[i] == nb_unique_digit[j]] = new_digit_nb[j]
+    new_digit_nb = np.cumsum(mask_digit_to_keep)
+    new_digit_nb -= 1
     
-    #print(np.shape(tab_img[i]))
+    for i in range(1, len(new_digit_nb)):
+        if new_digit_nb[len(new_digit_nb)-i] == new_digit_nb[len(new_digit_nb)-i-1]:
+            new_digit_nb[len(new_digit_nb)-i] = target_digit-1
     
-    img_1h = np.eye(target_digit)[tab_img[i]]
+    for i in range(0, len(tab_img)):
+        for j in range(0, len(nb_unique_digit)):
+            tab_img[i][tab_img[i] == nb_unique_digit[j]] = new_digit_nb[j]
     
-    #print(np.shape(img_1h))
+    return tab_img
     
+def convert_to_1h(img, gt):
+    img_1h = np.eye(target_digit)[img]
+    gt_1h = np.eye(target_class)[gt]
+
+    return img_1h, gt_1h
+
+def resize_to_target(img_1h, gt_1h):
     img_1h = resize(img_1h, (target_height, target_width, target_digit), order=1, anti_aliasing=True)
-    
-    print(np.shape(img_1h))
-    
-    #fig, (ax1, ax2) = plt.subplots(1, 2)
-    #ax1.imshow(tab_img[i])
-    #ax2.imshow(np.apply_along_axis(np.argmax, axis=2, arr=img_1h))
-    #plt.show()
-    #plt.clf()
-    
-    
-    #print(np.shape(tab_gt[i]))
-    
-    gt_1h = np.eye(target_class)[tab_gt[i]]
-    
-    #print(np.shape(gt_1h))
-    
     gt_1h = resize(gt_1h, (target_height, target_width, target_class), order=1, anti_aliasing=True)
     
-    print(np.shape(gt_1h))
+    return img_1h, gt_1h
     
-    #fig, (ax1, ax2) = plt.subplots(1, 2)
-    #ax1.imshow(tab_gt[i])
-    #ax2.imshow(np.apply_along_axis(np.argmax, axis=2, arr=gt_1h))
-    #plt.show()
-    #plt.clf()
+def plot_compare_input_1h(input, output_1h):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(input)
+    ax2.imshow(np.apply_along_axis(np.argmax, axis=2, arr=output_1h))
+    plt.show()
+    plt.clf()
     
-    pd_bbox = pd.read_pickle(os.path.join(dir_pd_bbox_reduced, list_filenames[i]).replace("npy", "pkl"))
-    
-    # Mask
-    np_bbox_anchor_mask = np.ones((np.shape(tab_img[i])[0], np.shape(tab_img[i])[1], 2*nb_anchors))
+def extract_anchor_mask(pd_bbox, img_shape):
+    np_bbox_anchor_mask = np.ones((img_shape[0], img_shape[1], 2*nb_anchors))
     for index, row in pd_bbox.iterrows():
         if row["class"] > 0:
             np_bbox_anchor_mask[row["top"]:row["bot"], row["left"]:row["right"], 2*(row["class"]-1)+1] = 0
@@ -120,12 +122,13 @@ for i in range(0, len(list_filenames)):
     
     np_bbox_anchor_mask = resize(np_bbox_anchor_mask, (target_height, target_width, 2*nb_anchors), order=1, anti_aliasing=True)
     
-    print(np.shape(np_bbox_anchor_mask))
-    
-    pd_bbox['left'] /= np.shape(tab_img[i])[1]
-    pd_bbox['right'] /= np.shape(tab_img[i])[1]
-    pd_bbox['top'] /= np.shape(tab_img[i])[0]
-    pd_bbox['bot'] /= np.shape(tab_img[i])[0]
+    return np_bbox_anchor_mask
+
+def extract_anchor_coordinates(pd_bbox, img_shape):
+    pd_bbox['left'] /= img_shape[1]
+    pd_bbox['right'] /= img_shape[1]
+    pd_bbox['top'] /= img_shape[0]
+    pd_bbox['bot'] /= img_shape[0]
     
     pd_bbox['np_left'] = round(pd_bbox['left']*target_width)
     pd_bbox['np_right'] = round(pd_bbox['right']*target_width)
@@ -136,9 +139,7 @@ for i in range(0, len(list_filenames)):
     pd_bbox['np_right'] = pd_bbox['np_right'].astype(int)
     pd_bbox['np_top'] = pd_bbox['np_top'].astype(int)
     pd_bbox['np_bot'] = pd_bbox['np_bot'].astype(int)
-        
-    # Coord
-    #np_bbox_anchor_coord = np.random.rand(target_height, target_width, 4*nb_anchors)
+
     np_bbox_anchor_coord = np.zeros((target_height, target_width, 4*nb_anchors))
     for index, row in pd_bbox.iterrows():
         if row["class"] > 0:
@@ -147,23 +148,63 @@ for i in range(0, len(list_filenames)):
             np_bbox_anchor_coord[row["np_top"]:row["np_bot"], row["np_left"]:row["np_right"], 4*(row["class"]-1)+2] = row["right"]
             np_bbox_anchor_coord[row["np_top"]:row["np_bot"], row["np_left"]:row["np_right"], 4*(row["class"]-1)+3] = row["bot"]
     
-    print(np.shape(np_bbox_anchor_coord))
+    return np_bbox_anchor_coord
     
-    #print(pd_bbox)
+def plot_anchor(gt_1h, np_bbox_anchor_mask, np_bbox_anchor_coord):    
+    fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(1, 7)
+    ax1.imshow(np.apply_along_axis(np.argmax, axis=2, arr=gt_1h))
+    ax2.imshow(np_bbox_anchor_mask[:, :, 2])
+    ax3.imshow(np_bbox_anchor_mask[:, :, 3])
+    ax4.imshow(np_bbox_anchor_coord[:, :, 4])
+    ax5.imshow(np_bbox_anchor_coord[:, :, 5])
+    ax6.imshow(np_bbox_anchor_coord[:, :, 6])
+    ax7.imshow(np_bbox_anchor_coord[:, :, 7])
+    plt.show()
+    plt.clf()
+
+
+if __name__ == "__main__":
+    list_filenames = [f for f in os.listdir(dir_np_chargrid_reduced) if os.path.isfile(os.path.join(dir_np_chargrid_reduced, f))]
     
-    #fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(1, 7)
-    #ax1.imshow(np.apply_along_axis(np.argmax, axis=2, arr=gt_1h))
-    #ax2.imshow(np_bbox_anchor_mask[:, :, 2])
-    #ax3.imshow(np_bbox_anchor_mask[:, :, 3])
-    #ax4.imshow(np_bbox_anchor_coord[:, :, 4])
-    #ax5.imshow(np_bbox_anchor_coord[:, :, 5])
-    #ax6.imshow(np_bbox_anchor_coord[:, :, 6])
-    #ax7.imshow(np_bbox_anchor_coord[:, :, 7])
-    #plt.show()
-    #plt.clf()
+    ## Load inputs
+    tab_img = []
+    tab_gt = []
+    for i in range(0, len(list_filenames)):
+        tab_img.append(np.load(os.path.join(dir_np_chargrid_reduced, list_filenames[i])))
+        tab_gt.append(np.load(os.path.join(dir_np_gt_reduced, list_filenames[i])))
     
-    ## Save        
-    #np.save(os.path.join(outdir_np_chargrid_1h, list_filenames[i]), img_1h)
-    #np.save(os.path.join(outdir_np_gt_1h, list_filenames[i]), gt_1h)
-    np.save(os.path.join(outdir_np_bbox_anchor_coord, list_filenames[i]), np_bbox_anchor_coord)
-    #np.save(os.path.join(outdir_np_bbox_anchor_mask, list_filenames[i]), np_bbox_anchor_mask)
+    print("tab_img shape=", np.shape(tab_img))
+    print("tab_gt shape=", np.shape(tab_gt))
+    
+    print_stats_img(tab_img)
+    print_stats_seg(tab_gt)
+    
+    tab_img = discard_digits_with_low_occurence(tab_img)
+    
+    for i in range(0, len(list_filenames)):
+        img_1h, gt_1h = convert_to_1h(tab_img[i], tab_gt[i])
+        #print(np.shape(img_1h))
+        #print(np.shape(gt_1h))
+        
+        img_1h, gt_1h = resize_to_target(img_1h, gt_1h)
+        print(np.shape(img_1h))
+        plot_compare_input_1h(tab_img[i], img_1h)
+        print(np.shape(gt_1h))
+        plot_compare_input_1h(tab_gt[i], gt_1h)
+        
+        ## Load input
+        pd_bbox = pd.read_pickle(os.path.join(dir_pd_bbox_reduced, list_filenames[i]).replace("npy", "pkl"))
+        
+        np_bbox_anchor_mask = extract_anchor_mask(pd_bbox, np.shape(tab_img[i]))
+        print(np.shape(np_bbox_anchor_mask))
+        
+        np_bbox_anchor_coord = extract_anchor_coordinates(pd_bbox, np.shape(tab_img[i]))
+        print(np.shape(np_bbox_anchor_coord))
+        
+        plot_anchor(gt_1h, np_bbox_anchor_mask, np_bbox_anchor_coord)
+
+        ## Save        
+        np.save(os.path.join(outdir_np_chargrid_1h, list_filenames[i]), img_1h)
+        np.save(os.path.join(outdir_np_gt_1h, list_filenames[i]), gt_1h)
+        np.save(os.path.join(outdir_np_bbox_anchor_coord, list_filenames[i]), np_bbox_anchor_coord)
+        np.save(os.path.join(outdir_np_bbox_anchor_mask, list_filenames[i]), np_bbox_anchor_mask)
